e6665b3075416a6afbf49ea4372e8fe0b315786d
ShaohaiLi <127905937+ShaohaiLi@users.noreply.github.com>
2024-07-25 22:44:48 -0600

    Update LogicGates.h with NOT and XOR gates

--- source/FourBitAdder/intern/LogicGates.h
+++ source/FourBitAdder/intern/LogicGates.h
@@ -3,6 +3,8 @@
  *
  *  Created on: Jun 14, 2020
  *      Author: brad
+ *
+ * update by Shaohai Li and add a NOT gate clas
  */
 
 #ifndef _LOGICGATES_H_
@@ -53,4 +55,40 @@ protected:
     virtual std::string repr();
 };
 
+// Add the NOT gate class
+class NOT: public AbstractGate
+{
+public:
+    /**
+     * Constructor for concrete class NOT.
+     */
+    NOT(const unsigned short int);
+    virtual ~NOT();
+
+protected:
+    std::string m_strID;
+    NAND m_nand; // Use NAND gate to implement NOT
+
+    virtual void update();
+    virtual std::string repr();
+};
+
+// You might also want to add an XOR gate for the half adder
+class XOR: public AbstractGate
+{
+public:
+    XOR(const unsigned short int);
+    virtual ~XOR();
+
+protected:
+    std::string m_strID;
+    // You'll need to implement XOR using the other gates
+    NAND m_nand1, m_nand2, m_nand3, m_nand4;
+
+    virtual void update();
+    virtual std::string repr();
+};
+
 #endif /* _LOGICGATES_H_ */
+
+
